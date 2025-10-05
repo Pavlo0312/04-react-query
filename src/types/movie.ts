c899077094da1942dbff609a907f8789a77f0ce9
Павло <pavlogyrev@gmail.com>
2025-10-05 17:24:29 +0200

    04-react-query

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -2,9 +2,12 @@ export interface Movie {
   id: number;
   title: string;
   poster_path: string | null;
+  backdrop_path?: string | null;
   release_date: string;
   vote_average: number;
+  overview?: string;
 }
+
 export interface MoviesResponse {
   page: number;
   results: Movie[];
