cb27fdc5280d20d8a6571e965b1f5f228ab8f46b
Павло <pavlogyrev@gmail.com>
2025-10-08 21:45:47 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/04-react-query/",
-  build: { outDir: "docs" },
+  base: "/",
+  build: { outDir: "dist" }, // стандартний вихід
 });
